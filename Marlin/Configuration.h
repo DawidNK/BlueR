589f184fcf7d9b746fd2e2ad17099010a28b96f0
“ils15” <igorleite-ils@hotmail.com>
2020-08-30 15:44:06 -0300

    Update Latest Marlin Stable

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -991,11 +991,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, -45, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -45, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 15
+#define PROBING_MARGIN 5
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
