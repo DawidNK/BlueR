f65eff443a3d48f623e5e4f91c7ba8d2a5685346
“ils15” <igorleite-ils@hotmail.com>
2020-09-21 22:33:53 -0300

    optimize PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -539,9 +539,9 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
     //Bluer
-    #define DEFAULT_bedKp 10.34
-    #define DEFAULT_bedKi 0.25
-    #define DEFAULT_bedKd 300.5
+   #define DEFAULT_bedKp 54.99
+   #define DEFAULT_bedKi 10.24
+   #define DEFAULT_bedKd 196.92
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
